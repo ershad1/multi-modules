61b60003ec4e10eeadaf0722273ec84705aba18d
ershad <ershad@gmail.com>
2022-12-01 15:47:30 +0600

    polishing

---
+++ user-service/src/main/java/com/sbicloud/securityconfig/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.sbicloud.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/users")
+                .hasAnyAuthority("SCOPE_profile")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+}
