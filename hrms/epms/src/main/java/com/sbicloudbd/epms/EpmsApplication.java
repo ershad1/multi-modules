5b10cc9b8b84ec5dd70bf2d9664d32094f23c615
ershad <ershad@gmail.com>
2022-12-05 19:05:38 +0600

    polishing

--- hrms/epms/src/main/java/com/sbicloudbd/epms/EpmsApplication.java
+++ hrms/epms/src/main/java/com/sbicloudbd/epms/EpmsApplication.java
@@ -1,9 +1,16 @@
 package com.sbicloudbd.epms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sbicloudbd")
+@ComponentScan("com.sbicloudbd")
+@EnableJpaRepositories(basePackages = "com.sbicloudbd")
+@EntityScan("com.sbicloudbd.*")
 public class EpmsApplication {
 
     public static void main(String[] args) {
